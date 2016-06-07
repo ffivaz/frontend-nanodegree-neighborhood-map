6af855f238f260b94b45fa896d3ea9df52416a7d
ffivaz <fabien.fivaz@unine.ch>
2016-06-07 10:49:58 +0200

    Bindings working well

--- js/ko_code.js
+++ js/ko_code.js
@@ -7,6 +7,7 @@ var locations = [
     {
         id: 1,
         name: 'Parc de l\'abeille',
+        longText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum molestie placerat. Maecenas venenatis libero eget tellus vehicula, vel feugiat purus vestibulum. Curabitur vitae sagittis risus. Curabitur urna ipsum, pharetra vel lectus at, tristique condimentum lacus. In vitae dolor at mauris mollis ullamcorper sed ac purus. Phasellus orci dui, convallis et neque quis, suscipit fringilla tortor. Cras faucibus magna eu libero rutrum, eget interdum lacus efficitur.',
         type: 'Park',
         link: null,
         lat: 47.10888,
@@ -15,6 +16,7 @@ var locations = [
     {
         id: 2,
         name: 'Bois du Petit Château',
+        longText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum molestie placerat. Maecenas venenatis libero eget tellus vehicula, vel feugiat purus vestibulum. Curabitur vitae sagittis risus. Curabitur urna ipsum, pharetra vel lectus at, tristique condimentum lacus. In vitae dolor at mauris mollis ullamcorper sed ac purus. Phasellus orci dui, convallis et neque quis, suscipit fringilla tortor. Cras faucibus magna eu libero rutrum, eget interdum lacus efficitur.',
         type: 'Recreation',
         link: 'http://www.chaux-de-fonds.ch/musees/zoo',
         lat: 47.10576,
@@ -23,6 +25,7 @@ var locations = [
     {
         id: 3,
         name: 'Stade de la Charrière',
+        longText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum molestie placerat. Maecenas venenatis libero eget tellus vehicula, vel feugiat purus vestibulum. Curabitur vitae sagittis risus. Curabitur urna ipsum, pharetra vel lectus at, tristique condimentum lacus. In vitae dolor at mauris mollis ullamcorper sed ac purus. Phasellus orci dui, convallis et neque quis, suscipit fringilla tortor. Cras faucibus magna eu libero rutrum, eget interdum lacus efficitur.',
         type: 'Sport',
         link: null,
         lat: 47.11075,
@@ -31,6 +34,7 @@ var locations = [
     {
         id: 4,
         name: 'Boulangerie du carrefour',
+        longText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum molestie placerat. Maecenas venenatis libero eget tellus vehicula, vel feugiat purus vestibulum. Curabitur vitae sagittis risus. Curabitur urna ipsum, pharetra vel lectus at, tristique condimentum lacus. In vitae dolor at mauris mollis ullamcorper sed ac purus. Phasellus orci dui, convallis et neque quis, suscipit fringilla tortor. Cras faucibus magna eu libero rutrum, eget interdum lacus efficitur.',
         type: 'Shopping',
         link: null,
         lat: 47.11009,
@@ -39,6 +43,7 @@ var locations = [
     {
         id: 5,
         name: 'Hôpital de La Chaux-de-Fonds',
+        longText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum molestie placerat. Maecenas venenatis libero eget tellus vehicula, vel feugiat purus vestibulum. Curabitur vitae sagittis risus. Curabitur urna ipsum, pharetra vel lectus at, tristique condimentum lacus. In vitae dolor at mauris mollis ullamcorper sed ac purus. Phasellus orci dui, convallis et neque quis, suscipit fringilla tortor. Cras faucibus magna eu libero rutrum, eget interdum lacus efficitur.',
         type: 'Service',
         link: null,
         lat: 47.11323,
@@ -47,6 +52,7 @@ var locations = [
     {
         id: 6,
         name: 'Poste de la Charrière',
+        longText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum molestie placerat. Maecenas venenatis libero eget tellus vehicula, vel feugiat purus vestibulum. Curabitur vitae sagittis risus. Curabitur urna ipsum, pharetra vel lectus at, tristique condimentum lacus. In vitae dolor at mauris mollis ullamcorper sed ac purus. Phasellus orci dui, convallis et neque quis, suscipit fringilla tortor. Cras faucibus magna eu libero rutrum, eget interdum lacus efficitur.',
         type: 'Service',
         link: null,
         lat: 47.10835,
@@ -55,31 +61,49 @@ var locations = [
 ];
 
 var Location = function(obj) {
+    var self = this;
     this.id = ko.observable(obj.id);
     this.name = ko.observable(obj.name);
+    this.longText = ko.observable(obj.longText);
     this.type = ko.observable(obj.type);
     this.long = ko.observable(obj.long);
     this.lat = ko.observable(obj.lat);
-    this.highlighted = ko.observable(obj.highlighted);
+    this.modalId = ko.observable("modal" + obj.id);
+    this.modalIdLink = ko.observable("#modal" + obj.id);
 };
 
 var viewModel = function() {
 
     var self = this;
 
-    this.locationList = ko.observableArray([]);
+    self.locationList = ko.observableArray([]);
 
     locations.forEach(function(obj) {
        self.locationList.push( new Location(obj) );
     });
 
-    self.focusedMarker = ko.observable();
+    self.clickedMarker = ko.observable();
 
-    self.highlighted = function (obj) {
-        self.focusedMarker(obj);
-        map.update(obj, self.focusedMarker());
+    self.clicked = function (obj) {
+        self.clickedMarker(obj);
+        $(self.clickedMarker().modalIdLink()).openModal();
+        this._mapMarker.setAnimation(google.maps.Animation.BOUNCE);
+        var that = this;
+        setTimeout(function () {
+            that._mapMarker.setAnimation(null);
+        }, 750);
     };
 
+    self.focusedMarker = ko.observable();
+
+    self.focused = function (obj) {
+        self.focusedMarker(obj);
+        this._mapMarker.setAnimation(google.maps.Animation.BOUNCE)
+    };
+
+    self.unfocused = function (obj) {
+        this._mapMarker.setAnimation(null);
+    };
 };
 
 /* Many people point out to a custom binding to manage the interaction between Google Maps and KnockOutJS.
@@ -90,21 +114,26 @@ ko.bindingHandlers.map = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
         var position = new google.maps.LatLng(allBindingsAccessor().latitude(), allBindingsAccessor().longitude());
         var marker = new google.maps.Marker({
+            id: allBindingsAccessor().id,
             map: allBindingsAccessor().map,
             position: position,
             title: allBindingsAccessor().name()
         });
 
         google.maps.event.addListener(marker, 'click', function () {
-            /* Here comes the code when the marker is clicked */
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            $(allBindingsAccessor().md()).openModal();
+            setTimeout(function () {
+                marker.setAnimation(null);
+            }, 750);
         });
 
         markers.push(marker);
         viewModel._mapMarker = marker;
     },
     update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        /*var latlng = new google.maps.LatLng(allBindingsAccessor().latitude(), allBindingsAccessor().longitude());
-        viewModel._mapMarker.setPosition(latlng);*/
+        var latlng = new google.maps.LatLng(allBindingsAccessor().latitude(), allBindingsAccessor().longitude());
+        viewModel._mapMarker.setPosition(latlng);
 
         var value = valueAccessor();
         if (ko.unwrap(value)) {
